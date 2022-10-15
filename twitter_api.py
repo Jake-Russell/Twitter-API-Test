43bc26e5e6b4d2440c2ca71b64ae236c5107dce9
Jake Russell <jake@megster.net>
2022-10-15 18:22:45 +0100

    Working PoC for searching Twitter for Tweets and exporting results to CSV

---
+++ twitter_api.py
@@ -0,0 +1,77 @@
+import tweepy
+import requests
+import configparser
+import pandas as pd
+
+# Read Configs
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+bearer_token = config['twitter']['bearer_token']
+
+client = tweepy.Client(bearer_token=bearer_token,
+                       return_type=requests.Response,
+                       wait_on_rate_limit=True)
+
+required_query_parameters = "lang:en"
+
+user_query = input("Enter the query: ")
+print("Now searching Twitter for Tweets with the following query: " + user_query)
+
+query = user_query + " " + required_query_parameters
+
+# query = 'from:jake_russell123'
+
+tweets = client.search_recent_tweets(query=query,
+                                     tweet_fields=['author_id', 'created_at', 'geo'],
+                                     expansions=['geo.place_id', 'author_id'],
+                                     place_fields=['contained_within', 'country', 'country_code', 'full_name',
+                                                   'id', 'name', 'place_type'],
+                                     max_results=10)
+
+# Save data as a dictionary
+tweets_dict = tweets.json()
+
+# Extract "data" value from dictionary
+has_tweet_data = True
+has_tweet_users = True
+has_tweet_places = True
+
+try:
+    tweets_data = tweets_dict['data']
+except KeyError:
+    print("No tweets matching: " + query)
+    has_tweet_data = False
+
+try:
+    tweets_users = tweets_dict['includes']['users']
+except KeyError:
+    print("No user data available")
+    has_tweet_users = False
+
+try:
+    tweets_places = tweets_dict['includes']['places']
+except KeyError:
+    print("No location data available")
+    has_tweet_places = False
+
+if has_tweet_data:
+    for data in tweets_dict['data']:
+        data_author_id = data['author_id']
+        if has_tweet_users:
+            for user in tweets_dict['includes']['users']:
+                if user['id'] == data['author_id']:
+                    data['author_username'] = user['username']
+        if has_tweet_places:
+            try:
+                for place in tweets_dict['includes']['places']:
+                    if place['id'] == data['geo']['place_id']:
+                        data['geo_name'] = place['full_name']
+            except KeyError:
+                print("This tweet has no location data")
+                data['geo_name'] = ''
+
+    # Transform to pandas Dataframe
+    df = pd.json_normalize(tweets_dict['data'])
+
+    df.to_csv("tweets.csv")
