033af06a0f17879806479a14d4be169221f20a58
Jake Russell <jake@megster.net>
2022-10-19 17:40:44 +0100

    Added c.Near to scraping_with_twint.py

--- scraping_with_twint.py
+++ scraping_with_twint.py
@@ -1,25 +1,14 @@
 import twint
-import pandas as pd
 
 c = twint.Config()
 
-c.search = ['Liz Truss']
-
-c.Search = ['Liz Truss']                # Topic
-# c.Pandas = True
+# c.Username = "JakeSnaps"
+c.Search = ['Liz Truss']
 # c.Location = True
-c.Near = "Loughborough"
-# c.Custom_csv = ["id", "user_id", "username", "date", "tweet"]
-# c.Location = True
-# c.Geo = '52.77129,-1.21470,100km'
+c.Near = "Swindon"
+# c.Geo = '52.77129,-1.21470,10000km'
 c.Limit = 1000                           # Number of Tweets to scrape
-c.Store_csv = True                      # Store tweets in a csv file
-c.Output = "twint_scraping_tweets.csv"  # Path to csv file
+c.Store_csv = True                       # Store tweets in a csv file
+c.Output = "twint_scraping_tweets.csv"   # Path to csv file
 
 twint.run.Search(c)
-
-# c.Username = "jakesnaps"
-# c.Store_csv = True
-# c.Output = "test.csv"
-#
-# twint.run.Lookup(c)
