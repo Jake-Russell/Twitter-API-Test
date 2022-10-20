34ce63281064dda6c8e2cb98319aeae825b59725
Jake Russell <jake@megster.net>
2022-10-20 18:02:23 +0100

    Read twint results into Tweet objects

--- scraping_with_twint.py
+++ scraping_with_twint.py
@@ -1,14 +1,35 @@
 import twint
+import csv
+from tweet import Tweet
+
+
+def reset_csv(filename, header):
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+
+
+output_file_name = "twint_csv_output.csv"
+csv_header = ['date', 'time', 'username', 'tweet', 'place', 'geo']
+reset_csv(output_file_name, csv_header)
 
 c = twint.Config()
 
 # c.Username = "JakeSnaps"
-c.Search = ['Liz Truss']
+c.Search = ['lang:en Liz Truss']
+c.Retweets = False
 # c.Location = True
-c.Near = "Swindon"
+# c.Near = "Swindon"
 # c.Geo = '52.77129,-1.21470,10000km'
-c.Limit = 100                           # Number of Tweets to scrape
+c.Limit = 20                           # Number of Tweets to scrape
 c.Store_csv = True                       # Store tweets in a csv file
-c.Output = "twint_scraping_tweets.csv"   # Path to csv file
-
+c.Custom["tweet"] = ["date", "time", "username", "tweet", "place", "geo"]
+c.Output = output_file_name   # Path to csv file
+c.Hide_output = True
 twint.run.Search(c)
+
+Tweet.instantiate_from_csv(output_file_name)
+
+print(f"\nNumber of Tweets = {len(Tweet.allTweets)}")
+for tweet in Tweet.allTweets:
+    print(tweet)
--- tweet.py
+++ tweet.py
@@ -1,12 +1,43 @@
+import csv
+from datetime import datetime
+
+
 class Tweet:
     allTweets = []
 
-    def __init__(self, text: str, username: str, location: str):
-        self.text = text
+    def __init__(self, date_time: datetime, username: str, text: str, location: str):
+        self.date_time = date_time
         self.username = username
+        self.text = text
         self.location = location
 
         Tweet.allTweets.append(self)
 
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.text}', {self.username}, {self.location})"
+        return f"{self.date_time}, {self.username}, {self.text}, {self.location}"
+
+    @classmethod
+    def instantiate_from_csv(cls, file: str):
+        with open(file, 'r') as f:
+            reader = csv.DictReader(f)
+            tweets = list(reader)
+
+        for tweet in tweets:
+            date_time = datetime.fromisoformat(f"{tweet.get('date')} {(tweet.get('time'))}")
+
+            Tweet(
+                date_time=date_time,
+                username=tweet.get('username'),
+                text=cls.__preprocess(tweet.get('tweet')),
+                location=tweet.get('place')
+            )
+
+    @staticmethod
+    def __preprocess(text):
+        new_text = []
+
+        for t in text.split(" "):
+            t = '@user' if t.startswith('@') and len(t) > 1 else t
+            t = 'http' if t.startswith('http') else t
+            new_text.append(t)
+        return " ".join(new_text)
