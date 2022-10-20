bc92044fefff27542e11121f06c258277d3aaa69
Jake Russell <jake@megster.net>
2022-10-20 16:43:38 +0100

    Changes to scraping_with_twint.py

--- scraping_with_twint.py
+++ scraping_with_twint.py
@@ -5,9 +5,9 @@ c = twint.Config()
 # c.Username = "JakeSnaps"
 c.Search = ['Liz Truss']
 # c.Location = True
-c.Near = "UK"
+c.Near = "Swindon"
 # c.Geo = '52.77129,-1.21470,10000km'
-c.Limit = 1000                           # Number of Tweets to scrape
+c.Limit = 100                           # Number of Tweets to scrape
 c.Store_csv = True                       # Store tweets in a csv file
 c.Output = "twint_scraping_tweets.csv"   # Path to csv file
 
